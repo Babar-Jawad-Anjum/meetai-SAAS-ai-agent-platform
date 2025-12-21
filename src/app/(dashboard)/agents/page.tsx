9c1f6a899ca60f66fa36b8954033d59a50c7941f
Babar Jawad Anjum <babarjawad010@gmail.com>
2025-12-22 02:19:13 +0500

    feat: agents form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -6,34 +6,51 @@ import AgentsView from "@/modules/agents/ui/views/agents-view";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import LoadingState from "@/components/loading-state";
 import ErrorState from "@/components/error-state";
+import AgentsListHeader from "@/modules/agents/ui/components/agents-list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+  
+   // Server side session check
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+  
+    if (!session) {
+      redirect("/sign-in");
+    }
+  
   // Using server component to prefetch the agents
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense
-        fallback={
-          <LoadingState
-            title="Loading Agents"
-            decription="This may take a few seconds"
-          />
-        }
-      >
-        <ErrorBoundary
+    <>
+      <AgentsListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense
           fallback={
-            <ErrorState
+            <LoadingState
               title="Loading Agents"
               decription="This may take a few seconds"
             />
           }
         >
-          <AgentsView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+          <ErrorBoundary
+            fallback={
+              <ErrorState
+                title="Loading Agents"
+                decription="This may take a few seconds"
+              />
+            }
+          >
+            <AgentsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 
